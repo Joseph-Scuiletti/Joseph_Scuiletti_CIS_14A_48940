9c5c9d1cb8efd76800f98831b48610869a30ac9d
Joseph-Scuiletti <Jospeh.scuiletti@gmail.com>
2015-09-28 10:55:44 -0700

    Signed-off-by: Joseph-Scuiletti <Jospeh.scuiletti@gmail.com>

---
+++ saveings.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+//Joseph Scuiletti
+//sept,28 2015
+//purpose: Explore writing finctions
+	function save1(pv,int,n){
+		for (var year=1;year<=n;year++){
+			pv*=(1+int);
+		}
+		return pv;
+	}
